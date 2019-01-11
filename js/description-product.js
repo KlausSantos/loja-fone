7d0491ce24999b42d5c51113f292dea3678df8ca
KlausSantos <klausantos82@gmail.com>
2019-01-11 19:52:50 -0300

    Correção desafio único - Aula [015] e finalizada a Aula [016]

--- js/description-product.js
+++ js/description-product.js
@@ -1,53 +1,48 @@
-/*
-1 - ter a janela (browser)
-2 - ter o html na mão 
-3 - pegar o coração
-4 - pegar o clique no coração
-5 - no momento que o usuário clicar, queremos mostra um texto na tela
-*/
-
 const $heart = window.document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".star");
+const positionLast = $stars.length - 1;
 
 $heart.addEventListener("click", handleClick);
 
-$stars.forEach(function($star) {
-  $star.addEventListener("click", handleClick);
+$stars.forEach(function($star, key) {
+  if (key == 0) {
+    $star.addEventListener("click", firstStar);
+  }
+
+  if (key > 0 && key < positionLast) {
+    $star.addEventListener("click", function() {
+      middleStar(key);
+    });
+  }
+
+  if (key == positionLast) {
+    $star.addEventListener("click", lastStar);
+  }
 });
 
-/* $stars[0].addEventListener("click", starClick0);
-$stars[1].addEventListener("click", starClick1);
-$stars[2].addEventListener("click", starClick2);
-$stars[3].addEventListener("click", starClick3);
-$stars[4].addEventListener("click", starClick4); */
+function firstStar() {
+  $stars.forEach(function($star) {
+    $star.classList.remove("-active");
+  });
+  this.classList.add("-active");
+}
+
+function middleStar(index) {
+  $stars.forEach(function($star, key) {
+    $star.classList.remove("-active");
+
+    if (key <= index) {
+      $star.classList.add("-active");
+    }
+  });
+}
+
+function lastStar() {
+  $stars.forEach(function($star) {
+    $star.classList.add("-active");
+  });
+}
 
 function handleClick() {
-  /*  if ($heart.classList.contains("-active")) {
-    $heart.classList.remove("-active");
-  } else {
-    $heart.classList.add("-active");
-  } */
-
   this.classList.toggle("-active");
 }
-//por conta do THIS não foi necessário o restante das linhas ... FELIZ
-/* 
-function handleStar() {
-  this.classList.toggle("-active");
-}
- */
-/* function starClick0() {
-  $stars[0].classList.toggle("-active");
-}
-function starClick1() {
-  $stars[1].classList.toggle("-active");
-}
-function starClick2() {
-  $stars[2].classList.toggle("-active");
-}
-function starClick3() {
-  $stars[3].classList.toggle("-active");
-}
-function starClick4() {
-  $stars[4].classList.toggle("-active");
-} */
