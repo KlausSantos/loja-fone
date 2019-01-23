6a2d2d694859e39f38ea531466b5bfc6bb71c924
KlausSantos <klausantos82@gmail.com>
2019-01-23 11:26:02 -0300

    inserido o icone de menu - aula 19.3

--- js/input-search.js
+++ js/input-search.js
@@ -1,8 +1,21 @@
 const $inputSearch = document.querySelector(".input-search");
 
-const $icon = $inputSearch.querySelector(".icon");
+const $open = $inputSearch.querySelector(".-open");
 
-$icon.addEventListener("click", event => {
+const $close = $inputSearch.querySelector(".-close");
+
+$open.addEventListener("click", event => {
   event.preventDefault();
+
   $inputSearch.classList.toggle("-active");
+  $open.classList.remove("-active");
+  $close.classList.add("-active");
+});
+
+$close.addEventListener("click", event => {
+  event.preventDefault();
+
+  $inputSearch.classList.toggle("-active");
+  $close.classList.remove("-active");
+  $open.classList.add("-active");
 });
